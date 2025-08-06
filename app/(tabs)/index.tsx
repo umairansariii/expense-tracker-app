b99f0d89831d5e775bd04755ca5e7ebb1c83eab3
umairansariii <umairansari.work@gmail.com>
2025-08-06 20:04:36 +0500

    Add transaction feed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { TransactionFeed } from '@/components/kit/TransactionFeed';
 import { ThemedView } from '@/components/ThemedView';
 import { StyleSheet, Text } from 'react-native';
 
@@ -5,6 +6,7 @@ export default function HomeScreen() {
   return (
     <ThemedView style={StyleSheet.absoluteFillObject} colorRole="surface">
       <Text>Home Screen</Text>
+      <TransactionFeed />
     </ThemedView>
   );
 }
